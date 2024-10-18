8a5638e3937cfed786fdb332e7636085510b4f9a
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2024-10-18 15:32:27 -0300

    Dynamic cards

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+(async function() {
+  const flashcards = await loadCards('../flashcards.json')
+
+  for (let flashcard of flashcards) {
+    generateCard(
+      flashcard.category,
+      flashcard.question,
+      flashcard.answer
+    )
+  }
+})()
+
+const cardContainer = document.getElementById('card-container')
+const cardTemplate = document.getElementById('card-template')
+  .content.querySelector('.card')
+
+async function loadCards(file) {
+  const flashcards = await fetch(file).then(res => res.json())
+  return Object.values(flashcards)
+}
+
+function generateCard(category, question, answer) {
+  const card = document.importNode(cardTemplate, true)
+  const categoryTag = card.querySelector('.category-tag')
+  const questionCard = card.querySelector('.question-card > p')
+  const answerCard = card.querySelector('.answer-card > p')
+
+  categoryTag.textContent = category
+  questionCard.textContent = question
+  answerCard.textContent = answer
+
+  card.addEventListener('click', function() {
+    this.classList.toggle('active')
+  })
+
+  cardContainer.appendChild(card)
+}
+
+
